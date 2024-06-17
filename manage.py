1938fcdde207218b1bb78648ffad4e4a71921402
Augustine Salas <augustinsalas@gmail.com>
2024-06-17 17:33:24 +0530

    f

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Schedule-Master.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Schedule_Master.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
